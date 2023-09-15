7bc1e2758ebcb08b80eba3c4890e24c299771daa
eli-mone <elimone.tech@gmail.com>
2023-09-15 17:22:57 +0300

    Initial Commit

---
+++ SwiftUiAdvanced/SwiftUiAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiAdvancedApp.swift
+//  SwiftUiAdvanced
+//
+//  Created by mohammed on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
